7532e165bb85b8bc5b8192bf5a7a26eca03ef8d2
wu_yiqiang <sutter.wu@itforce-tech.com>
2026-01-04 21:27:25 +0800

    new page

--- src/views/course/course_detail.vue
+++ src/views/course/course_detail.vue
@@ -2,20 +2,34 @@
   <section class="course_detail">
     <div class="title"><strong class="breadcum">选课程</strong> > <strong class="breadcum">课程详情</strong></div>
     <div class="contents">
-      <div class="leftbox"></div>
-      <hot_course />
+      <div class="leftbox">
+        <course_info :target="target" />
+      </div>
+      <hot_course :target="target" />
     </div>
   </section>
 </template>
 <script lang="ts" setup>
-import { reactive, watch, computed, ref } from 'vue'
+import { computed } from 'vue'
 import hot_course from './hot_course.vue'
+import course_info from './course_info.vue';
+import {courses} from './datas'
+const props = defineProps({
+  courseId: {
+    type: Number,
+    required: true,
+    default: () => {},
+  },
+})
+const target = computed(() => {
+  return courses?.find(item => item.id === props?.courseId)
+})
 </script>
 <style lang="scss" scoped>
 .course_detail {
   padding: 20px var(--padding-width);
-
   .title {
+    padding: 10px 0;
     .breadcum {
       font-size: 16px;
       font-weight: 600;
@@ -25,6 +39,13 @@ import hot_course from './hot_course.vue'
   .contents {
     display: flex;
     justify-content: space-between;
+    column-gap: 20px;
+    .leftbox {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      row-gap: 20px;
+    }
   }
 }
 </style>
---
+++ src/views/course/course_info.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class='CourseInfo'>
+    <div class="leftbox">
+        <img class="course" :src="image" alt="">
+        <img class="type" :src="typeImage" />
+    </div>
+    <div class="rightbox">
+        <div class="title">{{ target?.title }}</div>
+        <div class="sectitle"> {{ target?.secTitle }}</div>
+        <div class="year">考试年份：{{ target?.year }}</div>
+        <div class="year">班级容量：敬请期待</div>
+        <div class="year">开班日期：{{ target?.startDate }}</div>
+        <div class="year">上课事件：敬请期待</div>
+        <div class="year">上课地点：<div v-if="!target?.location?.length" class="empty">暂无</div>
+</div>
+    </div>
+  </section>
+</template>
+
+<script setup lang='ts'>
+import { computed } from 'vue';
+import { subjectType } from './datas';
+
+const props = defineProps({
+  target: {
+    type: Object,
+    required: true,
+    default: () => {},
+  },
+})
+const image = computed(() => {
+  return new URL(`../../assets/${props?.target?.image}`, import.meta.url).href
+})
+const typeImage = computed(() => {
+    let str = ''
+if (props?.target?.type === subjectType.GGK) str = 'course_center_ggk.png'
+  if (props?.target?.type === subjectType.QKK) str = 'course_center_qkk.png'
+  if (props?.target?.type === subjectType.TSK) str = 'course_center_tsk.png'
+  if (props?.target?.type === subjectType.ZYK) str = 'course_center_zyk.png'
+  return new URL(`../../assets/${str}`, import.meta.url).href
+})
+</script>
+<style scoped lang='scss'>
+.CourseInfo {
+    background-color: white;
+    border-radius: 8px;
+    padding: 20px;
+    display: flex;
+    height: fit-content;
+    column-gap: 30px;
+    .leftbox {
+        position: relative;
+        .course {
+            width: 360px;
+        }
+        .type {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            width: 80px;
+        }
+    }
+    .rightbox {
+        flex: 1;
+        .title  {
+            margin-bottom: 20px;
+            font-size: 20px;
+            font-weight: 500;
+        }
+        .sectitle {
+            font-size: 12px;
+            margin-bottom: 40px;
+        }
+        .year {
+            margin: 10px 0;
+            font-size: 14px;
+            color: grey;
+            display: flex;
+            align-items: center;
+        }
+          .empty {
+          padding: 8px;
+          font-size: 12px;
+          font-weight: 600;
+          border-radius: 4px;
+          color: #00baff;
+          background-color: #e1f7ff;
+          width: fit-content;
+        }
+    }
+}
+</style>
